9f9917d65bbf2a3e6b05fbda2357507b2db30e81
quangbx <quangnb8@gmail.com>
2019-06-01 23:15:31 +0700

    layout

--- app/src/main/java/com/quang/duoi_hinh_batchu/PlayActivity.java
+++ app/src/main/java/com/quang/duoi_hinh_batchu/PlayActivity.java
@@ -1,40 +1,40 @@
 package com.quang.duoi_hinh_batchu;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.view.WindowManager;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+import com.quang.duoi_hinh_batchu.R;
+
+public class PlayActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_play);
         initView();
+
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
     }
 
     private void initView() {
-        findViewById(R.id.iv_choi_thu).setOnClickListener(this);
+        findViewById(R.id.iv_back).setOnClickListener(this);
     }
 
     @Override
     public void onClick(View v) {
         switch (v.getId()) {
-            case R.id.iv_choi_thu:
-                goToPlay();
+            case R.id.btn_tiep:
+                break;
+            case R.id.iv_back:
+                onBackPressed();
                 break;
             default:
                 break;
         }
     }
 
-    private void goToPlay() {
-        Intent intent = new Intent();
-        intent.setClass(this, PlayActivity.class);
-        startActivity(intent);
-    }
+
 }
